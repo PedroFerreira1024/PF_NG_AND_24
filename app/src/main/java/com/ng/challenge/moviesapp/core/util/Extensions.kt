8c2e49b08ce3655e2f4fea061b72710fcfe14648
Pedro Ferreira <pedroferreira1024@gmail.com>
2024-06-28 15:17:20 +0100

    Added default value missing for image quality in extension function.

--- app/src/main/java/com/ng/challenge/moviesapp/core/util/Extensions.kt
+++ app/src/main/java/com/ng/challenge/moviesapp/core/util/Extensions.kt
@@ -1,6 +1,7 @@
 package com.ng.challenge.moviesapp.core.util
 
 import com.ng.challenge.moviesapp.BuildConfig
+import com.ng.challenge.moviesapp.movie_popular.data.mapper.IMAGE_QUALITY_LOW
 
-fun String?.toPostUrl(quality: String) = "${BuildConfig.BASE_URL_IMAGE+quality}$this"
+fun String?.toPostUrl(quality: String = IMAGE_QUALITY_LOW) = "${BuildConfig.BASE_URL_IMAGE+quality}$this"
 fun String?.toBackdropUrl() = "${BuildConfig.BASE_URL_IMAGE}$this"
